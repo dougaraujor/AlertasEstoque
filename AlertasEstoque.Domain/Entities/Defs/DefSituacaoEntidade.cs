c8512b34b07cd3ba02e73963c3b4d71867790778
dougaraujor <douglas.rocha@delage.com.br>
2026-03-19 22:28:24 -0300

    Ajustes pre camada de infra.

---
+++ AlertasEstoque.Domain/Entities/Defs/DefSituacaoEntidade.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AlertasEstoque.Domain.Entities.Defs;
+
+
+[Table("DefSituacaoEntidade")]
+public class DefSituacaoEntidade
+{
+    [Key]
+    public int IdSituacao { get; set; }
+    
+    public string Descricao { get; set; }
+}
---
+++ AlertasEstoque.Domain/Entities/Entidades/Entidades.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using AlertasEstoque.Domain.Entities.Defs;
+
+namespace AlertasEstoque.Domain.Entities.Entidades;
+
+[Table("Entidades")]
+public class Entidades 
+{
+    [Key]
+    public int Id { get; set; }
+    
+    public string RazaoSocial { get; set; }
+    
+    public string? Fantasia  {get; set; }
+    
+    public DefSituacaoEntidade IdSituacao{ get; set; }
+    
+    [Column(("Obs"))]
+    public string? Observacao { get; set; }
+    
+    public string? Imagem { get; set; }
+    
+    public DateTime? DataUltimoLogin { get; set; }
+    
+    public DateTime DataCriacao { get; set; }
+    
+    public DateTime? DataAlteracao { get; set; }
+    
+    
+}
